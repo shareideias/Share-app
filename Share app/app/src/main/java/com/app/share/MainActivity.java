e5db1f00d49a1339feeca939871cf3d29ad40492
João <joaovmontefusco@gmail.com>
2020-05-28 17:46:46 -0300

    first commit

---
+++ Share app/app/src/main/java/com/app/share/MainActivity.java
@@ -0,0 +1,42 @@
+package com.app.share;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static  int SPLASH_TIME_OUT = 4000;
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        myWebView.setWebViewClient(webViewClient);
+
+        myWebView.loadUrl("https://associacaoshare.com.br/login");
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
